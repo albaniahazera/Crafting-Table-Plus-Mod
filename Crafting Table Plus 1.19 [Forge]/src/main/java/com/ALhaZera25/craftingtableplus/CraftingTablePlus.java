3291253717c36a4909243c9ec3e3ff8843cdf8e4
albaniahazera <alzgamer2005@gmail.com>
2025-06-26 11:41:44 +0700

    change directory name

---
+++ Crafting Table Plus 1.19 [Forge]/src/main/java/com/ALhaZera25/craftingtableplus/CraftingTablePlus.java
@@ -0,0 +1,38 @@
+package com.ALhaZera25.craftingtableplus;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CraftingTablePlus.MOD_ID)
+public class CraftingTablePlus {
+
+    public static final String MOD_ID = "craftingtableplus";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CraftingTablePlus() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
